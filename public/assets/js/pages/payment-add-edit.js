9fa52c2d671d60cf239723cebf8a6280705427c1
Rivaldy Fauzan <rifalous@gmail.com>
2019-07-30 15:56:29 +0700

    Fix Topbar-Left Agar Lebih Responsif

--- public/assets/js/pages/payment-add-edit.js
+++ public/assets/js/pages/payment-add-edit.js
@@ -13,26 +13,17 @@ function getOutpatientDetail(outpatient_id) {
 	return res.responseJSON;
 }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-function getPasienData(pasien_id) {
-	var res = $.ajax({
-		url: SITE_URL+'/pasien/details-data/'+pasien_id,
-		type: 'get',
-		dataType: 'json',
-		async: false
-	});
+// function getPasienData(pasien_id) {
+// 	var res = $.ajax({
+// 		url: SITE_URL+'/pasien/details-data/'+pasien_id,
+// 		type: 'get',
+// 		dataType: 'json',
+// 		async: false
+// 	});
 
-	return res.responseJSON;
-}
+// 	return res.responseJSON;
+// }
 
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 function getOutpatient()
 {
 	var res = $.ajax({
@@ -45,36 +36,15 @@ function getOutpatient()
 	return res.responseJSON;
 }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-
-
->>>>>>> Stashed changes
-=======
-
-
->>>>>>> Stashed changes
 arr_outpatient = getOutpatient();
 
 $('.outpatient-id').select2({
     data: arr_outpatient
 }).on('change', function(){
 	var examination_outpatient = getOutpatientDetail($(this).val());
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+	//var pasien_data = getPasienData($(this).val());
 	$('[name="total_biaya"]').val(examination_outpatient.amount);
 	$('[name="pasien_id"]').val(examination_outpatient.pasien_id);
-=======
-=======
->>>>>>> Stashed changes
-	var pasien_data = getPasienData($(this).val());
-	$('[name="total_biaya"]').val(examination_outpatient.amount);
-	$('[name="pasien_id"]').val(examination_outpatient.pasien_id);
-	$('[name="address"]').val(pasien_data.address);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+	//$('[name="address"]').val(pasien_data.address);
 	// $('[name="doctor_id"]').val(examination_outpatient.doctor.name);
 });
