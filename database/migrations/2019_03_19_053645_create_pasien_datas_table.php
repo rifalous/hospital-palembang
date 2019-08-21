838c5aeb3a088e3415fac719dbf64ea107f5ef7d
Rivaldy Fauzan <rifalous@gmail.com>
2019-08-21 13:36:31 +0700

    Pembaruan Table Data Pasien

---
+++ database/migrations/2019_03_19_053645_create_pasien_datas_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasienDatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('pasien_datas');
+        Schema::create('pasien_datas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pasien_id');
+            $table->string('place');
+            $table->string('date_of_birth');
+            $table->string('gender');
+            $table->string('religion');   
+            $table->string('education');   
+            $table->string('work');   
+            $table->string('status');   
+            $table->string('blood_group');   
+            $table->string('address');  
+            $table->string('district_id'); 
+            $table->string('city_id'); 
+            $table->string('province_id'); 
+            $table->string('postal_code'); 
+            $table->string('identification_number'); 
+            $table->string('bpjs_number'); 
+            $table->string('phone');   
+            $table->string('father_name');   
+            $table->string('mother_name');   
+            $table->string('age');   
+            $table->string('age_father');   
+            $table->string('age_mother');   
+            $table->string('guardian_name');   
+            $table->string('guardian_address');
+            $table->string('family_relationship');
+            $table->string('photos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasien_datas');
+    }
+}
--- public/assets/js/pages/registration_outpatient.js
+++ public/assets/js/pages/registration_outpatient.js
@@ -2,26 +2,28 @@ var tbProgram;
 $(document).ready(function(){
 	tbProgram = $('#table-registration_outpatient').DataTable({
 		// order: [2, 'asc'],
-        ajax: SITE_URL + '/registration_outpatient/get_data',
-        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-        columns: [
-            //{ data: 'option', name: 'option', orderable: false,  class: 'text-center' },
-            { data: 'no_registrasi', name: 'no_registrasi' },
-            { data: 'pasien_name', name: 'pasien_name' },
-            { data: 'pasien_gender', name: 'pasien_gender' },
-            { data: 'pasien_age', name: 'pasien_age' },
-            { data: 'doctor_name', name: 'doctor_name'},
-            { data: 'tgl_periksa', name: 'tgl_periksa'},
-            { data: 'poliklinik', name: 'poliklinik'},
-            { data: 'disease', name: 'disease'},
-            { data: 'complaint', name: 'complaint'},
-            { data: 'actions', name: 'actions', searching: false, sorting: false, class: 'text-center' }
-
-        ],
-        	searchable: false,
-        	scrollCollapse: true,
-			scrollX: '100%'
-    });
+		ajax: SITE_URL + '/registration_outpatient/get_data',
+		headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+		columns: [
+				//{ data: 'option', name: 'option', orderable: false,  class: 'text-center' },
+				{ data: 'no_registrasi', name: 'no_registrasi' },
+				{ data: 'pasien_name', name: 'pasien_name' },
+				{ data: 'pasien_gender', name: 'pasien_gender' },
+				{ data: 'pasien_age', name: 'pasien_age' },
+				{ data: 'doctor_name', name: 'doctor_name'},
+				{ data: 'tgl_periksa', name: 'tgl_periksa'},
+				{ data: 'poliklinik', name: 'poliklinik'},
+				{ data: 'disease', name: 'disease'},
+				{ data: 'complaint', name: 'complaint'},
+				{ data: 'actions', name: 'actions', searching: false, sorting: false, class: 'text-center' }
+		],
+		searchable: false,
+		scrollCollapse: true,
+		scrollX: '100%',
+		drawCallback: function(){
+			$('[data-toggle="tooltip"]').tooltip();
+		}
+	});
 	$('#search').keyup(function(event){
 		    if(event.keyCode == 13){
 		       on_search();
@@ -32,55 +34,66 @@ $(document).ready(function(){
 		  on_clear();
 		});
 
-		$('#select-all').click(function(even){
-			if(this.checked) {
-				// Iterate each checkbox
-				$('[name="rowcheck[]"]').each(function() {
-				  this.checked = true;                        
-				});
-			} else {
-				$('[name="rowcheck[]"]').each(function() {
-				  this.checked = false;                        
-				});
-			}
-  	});
+	$('#select-all').click(function(even){
+		if(this.checked) {
+			// Iterate each checkbox
+			$('[name="rowcheck[]"]').each(function() {
+				this.checked = true;                        
+			});
+		} else {
+			$('[name="rowcheck[]"]').each(function() {
+				this.checked = false;                        
+			});
+		}
+	});
 
 	$('#btn-confirm').click(function(){
-
 		var id = [];
 		$('input[name="rowcheck[]"]:checked').each(function(){
 			id.push($(this).val());
 		});
 
-		$.ajax({
+		request('destroy/', id);
+	});
 
-			url: SITE_URL + '/registration_outpatient/destroy/',
-			type: 'DELETE',
-			data: {
-				id: id
-			},
-			headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-			beforeSend: function(){
-				$('#btn-confirm').attr('disabled', 'disabled');
-				$('#btn-confirm').html('<i class="fa fa-spinner fa-pulse"></i> Loading ...');
-			},
-			success: function(data){
-				show_notification(data.title, data.type, data.message);
-				$('#modal-delete-confirm').modal('hide');
-				on_search();
-			},
-			error: function(xhr, status, error) {
-				show_notification('Error', 'error', error);
-			},
-			complete:function () { 
-				$('#btn-confirm').html('Yes');
-				$('#btn-confirm').removeAttr('disabled');
-			}
-		});
-
+	$('#btn-confirm-by-id').click(function(){
+		const pasien_id = $(this).data('value');
+		request('remove/'+pasien_id, pasien_id);
 	});
 
 });
+
+function request(url, data) {
+	$.ajax({
+
+		url: SITE_URL + '/registration_outpatient/'+url,
+		type: 'DELETE',
+		data: {
+			id: data
+		},
+		headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+		beforeSend: function(){
+			$('#btn-confirm').attr('disabled', 'disabled');
+			$('#btn-confirm').html('<i class="fa fa-spinner fa-pulse"></i> Loading ...');
+			$('#btn-confirm-by-id').attr('disabled', 'disabled');
+			$('#btn-confirm-by-id').html('<i class="fa fa-spinner fa-pulse"></i> Loading ...');
+		},
+		success: function(data){
+			show_notification(data.title, data.type, data.message);
+			$('#modal-delete-confirm').modal('hide');
+			on_search();
+		},
+		error: function(xhr, status, error) {
+			show_notification('Error', 'error', error);
+		},
+		complete:function () { 
+			$('#btn-confirm').html('Ya');
+			$('#btn-confirm').removeAttr('disabled');
+			$('#btn-confirm-by-id').html('Ya');
+			$('#btn-confirm-by-id').removeAttr('disabled');
+		}
+	});
+}
 
 // search
 
@@ -145,13 +158,19 @@ function on_show()
 
 // delete
 
-function on_delete()
+function on_delete(pasien_id)
 {
-
-	if ($('input[name="rowcheck[]"]:checked').length == 0) {
-	    show_notification('Informasi', 'warning', 'Silahkan Pilih Data Dahulu!');
+	if (pasien_id != undefined) {
+		$('#btn-confirm').hide();
+		$('#btn-confirm-by-id').show();
+		$('#modal-delete-confirm').modal('show');
+    $('#btn-confirm-by-id').data('value', pasien_id);
 	} else {
-	    $('#modal-delete-confirm').modal('show');
+		if ($('input[name="rowcheck[]"]:checked').length == 0) {
+				show_notification('Informasi', 'warning', 'Silahkan Pilih Data Dahulu!');
+		} else {
+				$('#modal-delete-confirm').modal('show');
+		}
 	}
 
 }
